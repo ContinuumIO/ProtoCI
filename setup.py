f41f2961874722ffe86b29d239c220c121669e0e
Peter Steinberg <psteinberg@continuum.io>
2016-01-21 09:46:11 -0800

    fix package for 0.0.8

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def find_package_data():
 
 setup(
     name='protoci',
-    version='0.0.7',
+    version='0.0.8',
     author='Continuum Analytics',
     author_email='psteinberg@continuum.io',
     url='http://github.com/ContinuumIO/protoci',
