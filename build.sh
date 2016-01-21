fdccf96833bdb427f92660b92bc6f5a161b7ae65
Peter Steinberg <psteinberg@continuum.io>
2016-01-21 09:19:36 -0800

    fix conda packaging for 0.0.6

--- build.sh
+++ build.sh
@@ -1,8 +1,2 @@
-echo "Prepare to clone conda and conda-build for metadata reading..."
-mkdir conda_tmp
-git clone https://github.com/conda/conda-build conda_tmp/conda-build
-git clone https://github.com/conda/conda conda_tmp/conda
-set PYTHONPATH=$PYTHONPATH:./conda_tmp/conda-build:./conda_tmp/conda
-echo "PYTHONPATH below should include ./conda_tmp"
-echo $PYTHONPATH
+
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
