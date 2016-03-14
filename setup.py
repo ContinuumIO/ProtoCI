83beff00372386c4098a606446fb07ecb9d1013d
Peter Steinberg <psteinberg@continuum.io>
2016-03-14 11:20:18 -0700

    fix meta git url rev

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def find_package_data():
 
 setup(
     name='protoci',
-    version='0.0.40',
+    version='0.0.41',
     author='Continuum Analytics',
     author_email='psteinberg@continuum.io',
     url='http://github.com/ContinuumIO/protoci',
